ff23afda1504c64064a20cf612b8ae84a8af80c9
pillowhead <gabriel.belmonte@gmail.com>
2019-02-12 17:21:54 +0100

    publish directory build

---
+++ build/precache-manifest.2c238c45efcc46ee78de56276da3b55e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be80377dc0c966501b4a",
+    "url": "/static/js/main.be80377d.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "be80377dc0c966501b4a",
+    "url": "/static/css/main.6f08601c.chunk.css"
+  },
+  {
+    "revision": "1597baf12d61495b613e28ad43581ddb",
+    "url": "/index.html"
+  }
+];
